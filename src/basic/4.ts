af612e164bbab8c251b9bd5a0acf618b43d10bc6
Froyzend <froyzend1993@gmail.com>
2024-12-18 11:07:01 +0100

    init

--- src/basic/4.ts
+++ src/basic/4.ts
@@ -1,16 +1,3 @@
-/*function showMessage(message) {
-  console.log(message);
-}
-
-function calc(num1, num2) {
-  return num1 + num2;
-}
-
-function customError() {
-  throw new Error('Error');
-}
-*/
-
 function showMessage(message: string): void {
   console.log(message);
 }
